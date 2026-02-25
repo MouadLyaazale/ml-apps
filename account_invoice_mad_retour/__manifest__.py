3e90fcb026db019c84b85d1fec2c8dc8cdf0b2f3
MouadLyaazale <m.lyaazale@schielemaroc.com>
2026-02-25 03:01:49 +0100

    Change author and add price details in manifest
    Updated author name and added pricing information.

--- account_invoice_mad_retour/__manifest__.py
+++ account_invoice_mad_retour/__manifest__.py
@@ -3,7 +3,9 @@
     "version": "19.0.1.0.0",
     "summary": "Adds MAD total field and Retour sur BL on credit notes",
     "category": "Accounting",
-    "author": "Schiele",
+    "author": "Mouad Lyaazale",
+    "price": 20.99,
+    "currency": "EUR",
     "license": "LGPL-3",
     "depends": ["account", "sh_invoice_picking"],
     "data": [
