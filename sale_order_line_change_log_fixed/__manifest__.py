1ad6935736a7a4e67058d549cb11f5e1d1f6ba26
MouadLyaazale <m.lyaazale@schielemaroc.com>
2026-02-25 03:20:14 +0100

    Fix author and add price to manifest
    Updated author and pricing information in the manifest.

--- sale_order_line_change_log_fixed/__manifest__.py
+++ sale_order_line_change_log_fixed/__manifest__.py
@@ -1,8 +1,10 @@
 {
     "name": "Sale Order Line Change Log",
     "version": "19.0.1.0.2",
+    "author": "Mouad Lyaazale",
+    "price": 20.99,
+    "currency": "EUR",
     "summary": "Log des modifications des lignes de commandes de vente dans le chatter",
-    "author": "Mouad + ChatGPT",
     "depends": ["sale_management", "mail"],
     "data": [],
     "installable": True,
