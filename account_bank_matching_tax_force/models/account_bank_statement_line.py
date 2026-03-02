90412e7bcd4a8ba640158c4cc5cb4ca57412782f
MouadLyaazale <m.lyaazale@schielemaroc.com>
2026-03-02 02:40:43 +0100

    Delete account_bank_matching_tax_force directory

--- account_bank_matching_tax_force/models/account_bank_statement_line.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models
-
-class AccountBankStatementLine(models.Model):
-    _inherit = 'account.bank.statement.line'
-    # Bank statement line inheritance for future extensions
-
-
